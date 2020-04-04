71a3eaa038c55046ad6ef536f34170d1ef277713
kyobankatu <kentokentokentokento601@gmail.com>
2020-04-05 01:11:15 +0900

    “Test”

--- server.js
+++ server.js
@@ -68,12 +68,12 @@ client_dis.on('message', msg => {
 });
 
 function createEmoji(url,id){
-    client_dis.channels.cache.get('602424007530119171').send('メッセージ');
-    /*let guild_id = '602415458947301383';
+    //client_dis.channels.cache.get('602424007530119171').send('メッセージ');
+    let guild_id = '602415458947301383';
     let guild = client_dis.get_guild(guild_id);
     guild.createEmoji(url, id)
   .then(emoji => console.log(`Created new emoji with name ${emoji.name}`))
-  .catch(console.error);*/
+  .catch(console.error);
 }
 
 // Discordへの接続
