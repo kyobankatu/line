6744baa5bd378b2c63eeae24eb21c7b55f04c7f7
kyobankatu <kentokentokentokento601@gmail.com>
2020-04-05 00:44:43 +0900

    “Test”

--- server.js
+++ server.js
@@ -68,11 +68,11 @@ client_dis.on('message', msg => {
 });
 
 function createEmoji(url,id){
-    client_dis.channels.get('602415458947301383').send('CLEAR!'+url);
-    /*let guild = client_dis.get_guild(602415458947301383);
+    let guild_id = 602415458947301383;
+    let guild = client_dis.get_guild(guild_id);
     guild.createEmoji(url, id)
   .then(emoji => console.log(`Created new emoji with name ${emoji.name}`))
-  .catch(console.error);*/
+  .catch(console.error);
 }
 
 // Discordへの接続
