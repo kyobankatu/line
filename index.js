7a5f68f20729953703d3930e2e69462702a448ea
kyobankatu <kentokentokentokento601@gmail.com>
2020-04-05 00:08:25 +0900

    “Test”

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+// discord.js モジュールのインポート
 const Discord = require('discord.js');
+
+// Discord Clientのインスタンス作成
 const client = new Discord.Client();
 
+// 準備完了イベントのconsole.logで通知黒い画面に出る。
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log('ready...');
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
 
+// 後でここに追記します。
+
+
+// Discordへの接続
 client.login(process.env.BOT_TOKEN);
