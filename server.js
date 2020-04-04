420d7a4aa844c1dffac018fee07948c04944cb5b
kyobankatu <kentokentokentokento601@gmail.com>
2020-04-05 01:19:12 +0900

    “Test”

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ client_dis.on('message', msg => {
 function createEmoji(url,id){
     //client_dis.channels.cache.get('602424007530119171').send('メッセージ');
     let guild_id = '602415458947301383';
-    let guild = client_dis.get_guild(guild_id);
+    let guild = client.guilds.cache.get(guild_id);
     guild.createEmoji(url, id)
   .then(emoji => console.log(`Created new emoji with name ${emoji.name}`))
   .catch(console.error);
