e82b30045c5278e384f133c29a09b96b4d0cda1e
kyobankatu <kentokentokentokento601@gmail.com>
2020-04-05 00:13:34 +0900

    “Test”

--- server.js
+++ server.js
@@ -51,3 +51,19 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+});
+
+// Discordへの接続
+client.login(process.env.BOT_TOKEN);
